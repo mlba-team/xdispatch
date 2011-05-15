b00c4156fb2bea3a5fd39f88039e1db6a6b2f904
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2011-05-15 19:52:51 +0000

    * Made xdispatch::source::data return values to enable pointers as template arguments
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/trunk@144 9e4c620c-c391-4b63-a7fb-4924991ea895

--- include/xdispatch/source.h
+++ include/xdispatch/source.h
@@ -53,20 +53,20 @@ protected:
          source::data()
          */
         void ready(const boost::any& = boost::any());
-	/**
-	 Overload this method in case you are implementing
-	 a sourcetype based on an dispatch_source_t. This
-	 way users can access the native object by using
-	 source::native().
+        /**
+         Overload this method in case you are implementing
+         a sourcetype based on an dispatch_source_t. This
+         way users can access the native object by using
+         source::native().
 
          By default this is returning NULL
-	 */
-	virtual dispatch_source_t native();
+         */
+        virtual dispatch_source_t native();
 
 private:
-	void set_cb(source*); 
-	friend class source;
-	source* cb;
+        void set_cb(source*);
+        friend class source;
+        source* cb;
 };
 
 /**
@@ -79,68 +79,68 @@ private:
 class XDISPATCH_EXPORT source : public object {
 
 public:
-	/**
-	 Creates a new source waiting for the given source type
-	 */
-	source(sourcetype*);
-	~source();
+        /**
+         Creates a new source waiting for the given source type
+         */
+        source(sourcetype*);
+        ~source();
 
-	void resume();
-	void suspend();
+        void resume();
+        void suspend();
 
-	/**
-	 Sets the handler to dispatch each time the source
-	 becomes ready. You can use data() to obtain a pointer
-	 to data provided by the source and possibly being the
-	 reason for the handler to be dispatched.
-	 */
+        /**
+         Sets the handler to dispatch each time the source
+         becomes ready. You can use data() to obtain a pointer
+         to data provided by the source and possibly being the
+         reason for the handler to be dispatched.
+         */
     void handler(xdispatch::operation*);
 #ifdef XDISPATCH_HAS_BLOCKS
-	/**
-	 Sets the handler to dispatch each time the source
-	 becomes ready. You can use data() to obtain a pointer
-	 to data provided by the source and possibly being the
-	 reason for the handler to be dispatched.
-	 */
+        /**
+         Sets the handler to dispatch each time the source
+         becomes ready. You can use data() to obtain a pointer
+         to data provided by the source and possibly being the
+         reason for the handler to be dispatched.
+         */
     void handler(dispatch_block_t);
 #endif
     /**
       Sets the queue the handler will be executed on
       */
     void set_queue(const xdispatch::queue&);
-	/**
-	  @returns the data that is associated with the currently dispatched
-	   and executed handler. The returned data is defined by the sourcetype
-	   used.
+        /**
+          @returns the data that is associated with the currently dispatched
+           and executed handler. The returned data is defined by the sourcetype
+           used.
 
       Pass the type of the data to retrieve as template parameter. If the requested
       datatype cannot be provided, a bad_cast exception will be thrown.
 
-	  @remarks Calling this method from outside of a handler is undefined
-	  */
-        template <typename T> static T& data(){
+          @remarks Calling this method from outside of a handler is undefined
+          */
+        template <typename T> static T data(){
         try {
-            return boost::any_cast<T>(_data());
+            return boost::any_cast<T>(*_data());
         } catch (const boost::bad_any_cast&) {
             throw std::bad_cast();
         }
     }
 
-	/**
-	 @returns the native dispatch_object_t associated with this source.
-	  This depends on the sourcetype used and will normally return NULL
-	  */
+        /**
+         @returns the native dispatch_object_t associated with this source.
+          This depends on the sourcetype used and will normally return NULL
+          */
     virtual dispatch_object_t native() const;
 
 private:
-	source(const source&);
-	source& operator=(const source&);
+        source(const source&);
+        source& operator=(const source&);
     class pdata;
     pdata* d;
 
         void notify(const boost::any&);
-    static const boost::any* _data();
-	friend class sourcetype;
+        static const boost::any* _data();
+        friend class sourcetype;
 };
 
 __XDISPATCH_END_NAMESPACE
