611146a4d85f933d28d9a599aef1e9835b7e4a88
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2011-11-22 16:15:49 +0000

    * Restructured the packaging information
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/trunk@259 9e4c620c-c391-4b63-a7fb-4924991ea895

---
+++ package/version.cmake
@@ -0,0 +1,57 @@
+
+# Variables referring to the system the packaging took place at
+if(MZ_64BIT)
+    set( PACKAGE_ARCH 			"amd64")
+else()
+    set( PACKAGE_ARCH 			"i386")
+endif()
+if(MZ_IS_CLANG)
+    set( PACKAGE_COMPILER 		"clang")
+else()
+    if(MZ_IS_VS)
+        set( PACKAGE_COMPILER 	"msvc")
+    else()
+        set( PACKAGE_COMPILER 	"gcc")
+    endif()
+endif()
+
+# Variables describing the version and functionality of 
+# libxdispatch will be updated on each new version release
+
+set( XDISPATCH_VERSION_MAJOR 	0)
+set( XDISPATCH_VERSION_MINOR 	7)
+set( XDISPATCH_VERSION_PATCH 	0)
+set( XDISPATCH_VERSION_SUFFIX 	devel)
+set( XDISPATCH_LIBRARY_NAME		xdispatch)
+set( XDISPATCH_SUMMARY			"Userspace implementation of grand central dispatch")
+set( XDISPATCH_MAINTAINER		"Marius Zwicker")
+set( XDISPATCH_LICENSE			"Apache License, Version 2.0")
+set( XDISPATCH_DESCRIPTION		"Provides an userspace implementation of the grand central dispatch api introduced with Mac OS X 10.6. Additionally a platform independent C++ interface is included")
+set( XDISPATCH_VERSION 			${XDISPATCH_VERSION_MAJOR}.${XDISPATCH_VERSION_MINOR}.${XDISPATCH_VERSION_PATCH}${XDISPATCH_VERSION_SUFFIX})
+set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "userspace implementation of grand central dispatch")
+set( CPACK_PACKAGE_VENDOR 		"www.mlba-team.de")
+set( CPACK_RESOURCE_FILE_LICENSE 	"${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set( CPACK_RESOURCE_FILE_README 	"${CMAKE_CURRENT_SOURCE_DIR}/README.dox")
+set( CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.dox")
+
+# The components xdispatch is built of
+set( CPACK_COMPONENTS_ALL libxdispatch libQtDispatch)
+set( CPACK_COMPONENT_LIBXDISPATCH_DISPLAY_NAME "XDispatch")
+set( CPACK_COMPONENT_LIBXDISPATCH_DESCRIPTION "C++ API for Grand Central Dispatch")
+set( CPACK_COMPONENT_LIBQTDISPATCH_DISPLAY_NAME "QtDispatch")
+set( CPACK_COMPONENT_LIBQTDISPATCH_DESCRIPTION "Provides integration of Grand Central Dispatch into the Qt Event-Loop. Additionally a QThreadPool compatible API to Grand Central Dispatch is provided")
+set( CPACK_COMPONENT_LIBQTDISPATCH_DEPENDS libxdispatch)
+
+
+
+
+# CPack constants built from the other variables
+set( CPACK_PACKAGE_VERSION_MAJOR 	"${XDISPATCH_VERSION_MAJOR}")
+set( CPACK_PACKAGE_VERSION_MINOR 	"${XDISPATCH_VERSION_MINOR}")
+set( CPACK_PACKAGE_VERSION_PATCH 	"${XDISPATCH_VERSION_PATCH}")
+set( CPACK_PACKAGE_VERSION 			"${XDISPATCH_VERSION}")
+set( CPACK_PACKAGE_FILE_NAME 		"${XDISPATCH_LIBRARY_NAME}_${CPACK_PACKAGE_VERSION}_${CMAKE_SYSTEM_NAME}_${PACKAGE_COMPILER}_${PACKAGE_ARCH}")
+
+
+# some debugging output
+message("-- Package information for ${XDISPATCH_VERSION}")
