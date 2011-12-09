49899a0c14e51ec30a4c6f8210d8355212c15bba
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2011-12-09 16:11:00 +0000

    * Did some configuration changes concerning proposed components * Tried to work on proper packaging support for rpm and deb
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/trunk@264 9e4c620c-c391-4b63-a7fb-4924991ea895

--- package/version.cmake
+++ package/version.cmake
@@ -32,16 +32,27 @@ set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "userspace implementation of grand centra
 set( CPACK_PACKAGE_VENDOR 		"www.mlba-team.de")
 set( CPACK_RESOURCE_FILE_LICENSE 	"${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
 set( CPACK_RESOURCE_FILE_README 	"${CMAKE_CURRENT_SOURCE_DIR}/README.dox")
-set( CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.dox")
+set( CPACK_PACKAGE_DESCRIPTION_FILE     "${CMAKE_CURRENT_SOURCE_DIR}/README.dox")
 
 # The components xdispatch is built of
-set( CPACK_COMPONENTS_ALL libxdispatch libQtDispatch)
+set( CPACK_COMPONENTS_ALL libdispatch libdispatch-dev libxdispatch libxdispatch-dev libQtDispatch libQtDispatch-dev )
+set( CPACK_COMPONENT_LIBDISPATCH_DISPLAY_NAME "Grand Central Dispatch")
+set( CPACK_COMPONENT_LIBDISPATCH_DESCRIPTION "user space implementation of the Grand Central Dispatch API")
+set( CPACK_COMPONENT_LIBDISPATCH-DEV_DISPLAY_NAME "Grand Central Dispatch - Development Files")
+set( CPACK_COMPONENT_LIBDISPATCH-DEV_DESCRIPTION "user space implementation of the Grand Central Dispatch API")
+set( CPACK_COMPONENT_LIBDISPATCH-DEV_DEPENDS libdispatch)
 set( CPACK_COMPONENT_LIBXDISPATCH_DISPLAY_NAME "XDispatch")
 set( CPACK_COMPONENT_LIBXDISPATCH_DESCRIPTION "C++ API for Grand Central Dispatch")
+set( CPACK_COMPONENT_LIBXDISPATCH_DEPENDS libdispatch)
+set( CPACK_COMPONENT_LIBXDISPATCH-DEV_DISPLAY_NAME "XDispatch - Development Files")
+set( CPACK_COMPONENT_LIBXDISPATCH-DEV_DESCRIPTION "C++ API for Grand Central Dispatch")
+set( CPACK_COMPONENT_LIBXDISPATCH-DEV_DEPENDS libxdispatch)
 set( CPACK_COMPONENT_LIBQTDISPATCH_DISPLAY_NAME "QtDispatch")
 set( CPACK_COMPONENT_LIBQTDISPATCH_DESCRIPTION "Provides integration of Grand Central Dispatch into the Qt Event-Loop. Additionally a QThreadPool compatible API to Grand Central Dispatch is provided")
 set( CPACK_COMPONENT_LIBQTDISPATCH_DEPENDS libxdispatch)
-
+set( CPACK_COMPONENT_LIBQTDISPATCH-DEV_DISPLAY_NAME "QtDispatch - Development Files")
+set( CPACK_COMPONENT_LIBQTDISPATCH-DEV_DESCRIPTION "Provides integration of Grand Central Dispatch into the Qt Event-Loop. Additionally a QThreadPool compatible API to Grand Central Dispatch is provided")
+set( CPACK_COMPONENT_LIBQTDISPATCH-DEV_DEPENDS libQtDispatch)
 
 
 
@@ -49,9 +60,9 @@ set( CPACK_COMPONENT_LIBQTDISPATCH_DEPENDS libxdispatch)
 set( CPACK_PACKAGE_VERSION_MAJOR 	"${XDISPATCH_VERSION_MAJOR}")
 set( CPACK_PACKAGE_VERSION_MINOR 	"${XDISPATCH_VERSION_MINOR}")
 set( CPACK_PACKAGE_VERSION_PATCH 	"${XDISPATCH_VERSION_PATCH}")
-set( CPACK_PACKAGE_VERSION 			"${XDISPATCH_VERSION}")
+set( CPACK_PACKAGE_VERSION 		"${XDISPATCH_VERSION}")
 set( CPACK_PACKAGE_FILE_NAME 		"${XDISPATCH_LIBRARY_NAME}_${CPACK_PACKAGE_VERSION}_${CMAKE_SYSTEM_NAME}_${PACKAGE_COMPILER}_${PACKAGE_ARCH}")
 
 
 # some debugging output
-message("-- Package information for ${XDISPATCH_VERSION}")
+message("-- version information for '${XDISPATCH_VERSION}'")
