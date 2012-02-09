56144264192814ac90a7dd2b3f3126e5525fd26b
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-02-09 18:19:04 +0000

    * Tweaked debian package config to fit ubuntu and the requirements of a launchpad ppa
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/trunk@281 9e4c620c-c391-4b63-a7fb-4924991ea895

---
+++ package/ubuntu/package.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+VERSION=$1
+BUILDDIR=/tmp/xdispatch
+
+# copy sources to clean target
+echo -- create $BUILDDIR
+mkdir -p $BUILDDIR
+echo -- copy sources to $BUILDDIR/$VERSION
+cp -rf . $BUILDDIR/$VERSION
+CURRENT=`pwd`
+cd $BUILDDIR/$VERSION
+# remove .svn directories (if any)
+echo -- clean svn information
+rm -rf `find . -type d -name .svn`
+# delete old build files
+echo -- cleaning build directories
+rm -rf Build/QtCreator_ProjectFiles Build/*MakeFiles Build/Docs Build/VS10_ProjectFiles*
+# copy debian packaging information
+echo -- copy packaging information
+cp -rf package/ubuntu debian
+
+# build src package
+echo -- build source package
+dpkg-buildpackage -us -uc -S -rfakeroot
+
+# build binary packages
+echo -- build binaries
+
+sudo pbuilder build --buildresult $BUILDDIR ../*.dsc 
+
+# copy binaries back to source dir
+echo -- retrieve packages
+cp *.deb *.dsc *.tar.gz *.changes $CURRENT/../
+cd ..
+lintian *.deb
+cp *.deb *.dsc *.tar.gz *.changes $CURRENT/../
+
+# cleanup
+echo -- cleanup
+cd $CURRENT
+rm -rf $BUILDDIR
+
+echo -- DONE
+
