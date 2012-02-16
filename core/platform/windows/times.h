c4091b721dae3a6fb145bbb9d913b40d23d7390e
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-02-16 16:14:16 +0000

    * Improved the implementation of gettimeofday to mat the posix variant more closely * Reduced the needed calls within the time shim
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/branches/stable@290 9e4c620c-c391-4b63-a7fb-4924991ea895

--- core/platform/windows/times.h
+++ core/platform/windows/times.h
@@ -31,7 +31,7 @@ static int gettimeofday(struct timeval *tv, struct timezone *tz)
     FILETIME ft;
 
     // Initialize the present time to 0 and the timezone to UTC
-    unsigned __int64 tmpres = 0;
+    ULARGE_INTEGER tmpres;
     static int tzflag = 0;
 
     if (NULL != tv)
@@ -39,27 +39,27 @@ static int gettimeofday(struct timeval *tv, struct timezone *tz)
         GetSystemTimeAsFileTime(&ft);
 
         // The GetSystemTimeAsFileTime returns the number of 100 nanosecond
-        // intervals since Jan 1, 1601 in a structure. Copy the high bits to
-        // the 64 bit tmpres, shift it left by 32 then or in the low 32 bits.
-        tmpres |= ft.dwHighDateTime;
-        tmpres <<= 32;
-        tmpres |= ft.dwLowDateTime;
+        // intervals since Jan 1, 1601 in a structure.
+        tmpres.LowPart  = ft.dwLowDateTime;
+        tmpres.HighPart = ft.dwHighDateTime;        
 
         // Convert to microseconds by dividing by 10
-        tmpres /= 10;
+        tmpres.QuadPart /= 10;
 
         // The Unix epoch starts on Jan 1 1970.  Need to subtract the difference
         // in seconds from Jan 1 1601.
-        tmpres -= DELTA_EPOCH_IN_MICROSECS;
+        tmpres.QuadPart -= DELTA_EPOCH_IN_MICROSECS;
 
         // Finally change microseconds to seconds and place in the seconds value.
         // The modulus picks up the microseconds.
-        tv->tv_sec = (long)(tmpres / 1000000UL);
-        tv->tv_usec = (long)(tmpres % 1000000UL);
+        tv->tv_sec = (long)(tmpres.QuadPart / 1000000UL);
+        tv->tv_usec = (long)(tmpres.QuadPart % 1000000UL);
     }
 
     if (NULL != tz)
     {
+        abort();
+        /*
         if (!tzflag)
         {
             _tzset();
@@ -69,8 +69,10 @@ static int gettimeofday(struct timeval *tv, struct timezone *tz)
         // Adjust for the timezone west of Greenwich
         tz->tz_minuteswest = _timezone / 60;
         tz->tz_dsttime = _daylight;
+        */
     }
 
     return 0;
 }
+
 #endif /* DISPATCH_WIN_TIMES_ */
