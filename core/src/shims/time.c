4f872ccd93f944c6f9c426a1fd7057cedaa301eb
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-02-19 19:11:58 +0000

    * Added missing #ifdef code guard discovered after testing the latest fixes to the Windows version on Linux.
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/branches/stable@297 9e4c620c-c391-4b63-a7fb-4924991ea895

--- core/src/shims/time.c
+++ core/src/shims/time.c
@@ -33,13 +33,15 @@ _dispatch_get_host_time_init(void *context DISPATCH_UNUSED)
 	_dispatch_host_time_data.frac = tbi.numer;
 	_dispatch_host_time_data.frac /= tbi.denom;
 	_dispatch_host_time_data.ratio_1_to_1 = (tbi.numer == tbi.denom);
-#else // TARGET_OS_WIN32 
+#else // TARGET_OS_WIN32 || TARGET_OS_LINUX
+# if TARGET_OS_WIN32
     LARGE_INTEGER freq;
+    dispatch_assume(QueryPerformanceFrequency(&freq));
+    _dispatch_host_time_data.win_time_factor = (long double)NSEC_PER_SEC / (long double)freq.QuadPart;
+# endif
 
 	_dispatch_host_time_data.frac = 1.0;
 	_dispatch_host_time_data.ratio_1_to_1 = 1;
-	dispatch_assume(QueryPerformanceFrequency(&freq));
-	_dispatch_host_time_data.win_time_factor = (long double)NSEC_PER_SEC / (long double)freq.QuadPart;
 #endif
 }
 
