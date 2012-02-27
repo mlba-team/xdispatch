b279e777a67d92b9145384f9260c43f5342a65e5
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-02-27 21:51:52 +0000

    * Port improvements to packaging scripts from ubuntu to debian
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/trunk@330 9e4c620c-c391-4b63-a7fb-4924991ea895

--- package/debian/deb.cmake
+++ package/debian/deb.cmake
@@ -8,8 +8,15 @@ set( CPACK_DEBIAN_PACKAGE_SECTION "Devel")
 set( CPACK_DEBIAN_PACKAGE_VERSION "${XDISPATCH_VERSION}-${PACKAGE_COMPILER}")
 set( CPACK_DEBIAN_COMPONENT_INSTALL ON) # enable me as soon as component based deb files work with cmake
 
-add_custom_target(debian ./package/debian/package.sh xdispatch-${XDISPATCH_VERSION}
-	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} )
+add_custom_target(
+	debian ./package/debian/package.sh xdispatch-${XDISPATCH_VERSION}
+	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+)
+
+configure_file( 
+    ${CMAKE_CURRENT_SOURCE_DIR}/package/debian/changelog.in
+    ${CMAKE_CURRENT_SOURCE_DIR}/package/debian/changelog
+)
 	
 message("-- Configured generator 'DEB (Debian)'")
 set( XDISPATCH_CPACK ${XDISPATCH_CPACK} DEB )
