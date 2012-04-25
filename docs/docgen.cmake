1eebfa6c31a997d55c2103b5486952afbe97d4d5
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-04-25 11:25:21 +0000

    Set target directory for documentation on OS X, fixed some typos
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/trunk@395 9e4c620c-c391-4b63-a7fb-4924991ea895

--- docs/docgen.cmake
+++ docs/docgen.cmake
@@ -17,8 +17,10 @@ set(DOXYGEN_DOT_FOUND NO)
 set(DOXYGEN_DOT_EXECUTABLE "")
 
 if(DOXYGEN_FOUND)
-	configure_file(${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile
-				${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile)
+		configure_file(
+			${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile
+			${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile
+		)
 
         # add to the default target
         if( MZ_IS_RELEASE )
@@ -35,19 +37,22 @@ if(DOXYGEN_FOUND)
 			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 		)
 		
-	# create a Qt Assistant Docset
-	add_custom_command(TARGET lemon_docs POST_BUILD
-		COMMAND ${QT_BINARY_DIR}/qhelpgenerator index.qhp -o ${EXECUTABLE_OUTPUT_PATH}/com.mlba.lemon.qch
-		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/docs/html
-		COMMENT "Creating QT Docset"
+		# create a Qt Assistant Docset
+		add_custom_command(TARGET lemon_docs POST_BUILD
+			COMMAND ${QT_BINARY_DIR}/qhelpgenerator index.qhp -o ${EXECUTABLE_OUTPUT_PATH}/de.mlba-team.xdispatch.qch
+			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/docs/html
+			COMMENT "Creating QT Docset"
         )
 
         # the install target for the docs
         if(APPLE)
             install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/docs/html
-                        DESTINATION "Library/Frameworks/xdispatch.framework/docs"
+                        DESTINATION "Library/Documentation/xdispatch"
                         COMPONENT xdispatch-doc
                         PATTERN "*.svn" EXCLUDE)
+            install(FILES LICENSE.rtf NOTICE README.rtf BUGS
+            			DESTINATION "Library/Documentation/xdispatch"
+            			COMPONENT xdispatch-doc)
         elseif(UNIX)
             install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/docs/html
                         DESTINATION share/doc/xdispatch-doc
@@ -60,7 +65,7 @@ if(DOXYGEN_FOUND)
                         PATTERN "*.svn" EXCLUDE)
         endif()
 
-	message("-- Found Doxygen, adding Documentation for lemon (make lemon_docs)")
+	message("-- Found Doxygen, adding Documentation for xdispatch (make xdispatch_docs)")
 else()
-	message("-- No Doxygen found, skipping Documentation for lemon")
+	message("-- No Doxygen found, skipping Documentation for xdispatch")
 endif()
