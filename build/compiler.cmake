ea8db492e4590902ec0bdae5fca6a2b444ef379d
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-10-05 11:19:27 +0000

    Update to mz-cmaketools (r459)
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/branches/stable@460 9e4c620c-c391-4b63-a7fb-4924991ea895

--- build/compiler.cmake
+++ build/compiler.cmake
@@ -1,26 +1,3 @@
-##
-# Copyright (c) 2008-2012 Marius Zwicker
-# All rights reserved.
-# 
-# @LICENSE_HEADER_START:Apache@
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
-# http://www.mlba-team.de
-# 
-# @LICENSE_HEADER_END:Apache@
-##
-
 ##################################################
 #
 #	BUILD/COMPILER.CMAKE
@@ -31,6 +8,7 @@
 # 	compiler variables. If available features
 #	for c++0x will be enabled automatically
 #
+#	(c) 2010-2012 Marius Zwicker
 #
 #
 # PROVIDED CMAKE VARIABLES
@@ -208,7 +186,7 @@ function(__MZ_COMPILER_IS_CLANG _OUTPUT _OUTPUT_VERSION)
   endif()
 
   string(
-    REGEX REPLACE "clang version ([0-9])\\.([0-9])(\\.[0-9])? \(.+\)" "\\1\\2"
+    REGEX REPLACE "(.*) version ([0-9])\\.([0-9])(\\.[0-9])?(\\-[0-9])?(~mlba)? \(.+\)" "\\2\\3"
     _MZ_CLANG_VERSION ${_MZ_CLANG_VERSION}
   )
   
@@ -242,17 +220,17 @@ if(NOT MZ_COMPILER_TEST_HAS_RUN)
 		set(WINDOWS TRUE CACHE INTERNAL WINDOWS )
 	endif()
 	
-    # clang is gcc compatible but still different
-    __MZ_COMPILER_IS_CLANG( _MZ_TEST_CLANG COMPILER_VERSION )
-    if( _MZ_TEST_CLANG )
-        	mz_message("compiler is clang")
-        set(MZ_IS_CLANG TRUE CACHE INTERNAL MZ_IS_CLANG)
-    endif()	
+  # clang is gcc compatible but still different
+  __MZ_COMPILER_IS_CLANG( _MZ_TEST_CLANG COMPILER_VERSION )
+  if( _MZ_TEST_CLANG )
+    	mz_message("compiler is clang")
+      set(MZ_IS_CLANG TRUE CACHE INTERNAL MZ_IS_CLANG)
+  endif()	
 	
 	# gnu compiler
 	#message("IS_GCC ${CMAKE_COMPILER_IS_GNU_CC}")
 	if(UNIX OR MINGW)
-        mz_message("GCC compatible compiler found")
+    mz_message("GCC compatible compiler found")
 		
 		set(MZ_IS_GCC TRUE CACHE INTERNAL MZ_IS_GCC)
 		
@@ -268,17 +246,17 @@ if(NOT MZ_COMPILER_TEST_HAS_RUN)
 				__Boost_MZ_COMPILER_DUMPVERSION(COMPILER_VERSION)
 		endif()
 		mz_message("compiler version ${COMPILER_VERSION}")
-		if(COMPILER_VERSION STRGREATER "45")
+		if(NOT MZ_IS_CLANG AND COMPILER_VERSION STRGREATER "45")
 			mz_message("C++11 support detected")
 			set(MZ_HAS_CXX0X TRUE CACHE INTERNAL MZ_HAS_CXX0X)
 			set(MZ_HAS_CXX11 TRUE CACHE INTERNAL MZ_HAS_CXX11)
-		elseif(COMPILER_VERSION STRGREATER "44")
+		elseif(NOT MZ_IS_CLANG AND COMPILER_VERSION STRGREATER "44")
 			mz_message("experimental C++0x support detected")
 			set(MZ_HAS_EXPERIMENTAL_CXX0X TRUE CACHE BOOL MZ_HAS_EXPERIMENTAL_CXX0X)
 			set(MZ_HAS_CXX0X TRUE CACHE BOOL MZ_HAS_CXX0X)
 			set(MZ_HAS_CXX11 TRUE CACHE BOOL MZ_HAS_CXX11)
 		elseif(MZ_IS_CLANG AND COMPILER_VERSION STRGREATER "29")
-			mz_message("C++11 support detected")
+			mz_message("clang 3.1+ with C++11 support detected")
 			set(MZ_HAS_CXX0X TRUE CACHE INTERNAL MZ_HAS_CXX0X)
 			set(MZ_HAS_CXX11 TRUE CACHE INTERNAL MZ_HAS_CXX11)		
 		endif()
@@ -313,26 +291,27 @@ if(NOT MZ_COMPILER_TEST_HAS_RUN)
 		set(MZ_32BIT ON CACHE INTERNAL MZ_32BIT)
 	endif()
 
-        # configured build type
-        # NOTE: This can be overriden e.g. on Visual Studio
-        if(CMAKE_BUILD_TYPE STREQUAL "Release")
-            set(MZ_IS_RELEASE TRUE CACHE INTERNAL MZ_IS_RELEASE)
-            mz_debug_message("CMake run in release mode")
-        endif()
+  # configured build type
+  # NOTE: This can be overriden e.g. on Visual Studio
+  if(CMAKE_BUILD_TYPE STREQUAL "Release")
+    set(MZ_IS_RELEASE TRUE CACHE INTERNAL MZ_IS_RELEASE)
+    mz_debug_message("CMake run in release mode")
+  endif()
 
 endif() #MZ_COMPILER_TEST_HAS_RUN
 
 # determine current date and time
 if(WINDOWS)
-    execute_process(COMMAND "date" "/T" OUTPUT_VARIABLE MZ_DATE_STRING)
+  execute_process(COMMAND "date" "/T" OUTPUT_VARIABLE MZ_DATE_STRING)
 else() # Sun, 11 Dec 2011 12:07:00 +0200
-    execute_process(COMMAND "date" "+%a, %d %b %Y %T %z" OUTPUT_VARIABLE MZ_DATE_STRING)
+  execute_process(COMMAND "date" "+%a, %d %b %Y %T %z" OUTPUT_VARIABLE MZ_DATE_STRING)
+  string(REPLACE "\n" "" MZ_DATE_STRING "${MZ_DATE_STRING}")
 endif()
 mz_message("Today is: ${MZ_DATE_STRING}")
 
 # optional C++0x/c++11 features on gcc (on vs2010 this is enabled by default)
 if(MZ_IS_GCC AND MZ_HAS_CXX0X) # AND NOT DARWIN)
-    mz_add_cxx_flag(GCC -std=gnu++0x)
+  mz_add_cxx_flag(GCC -std=gnu++0x)
 	mz_message("forcing C++11 support on this platform")
 endif()
 
@@ -340,11 +319,11 @@ endif()
 mz_add_definition(${CMAKE_SYSTEM_PROCESSOR}=1)
 mz_add_flag(GCC -Wall -Werror -Wno-unused-function)
 if(WINDOWS)
-    mz_add_definition(WIN32=1 WINDOWS=1)
+  mz_add_definition(WIN32=1 WINDOWS=1)
 elseif(IOS)
 	mz_add_definition(IOS=1)
 else()
-    mz_add_definition(${CMAKE_SYSTEM_NAME}=1)
+  mz_add_definition(${CMAKE_SYSTEM_NAME}=1)
 endif()
 
 
@@ -355,11 +334,11 @@ if(MZ_IS_GCC)
 	SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
 		
 	if(WINDOWS)
-        if(MZ_64BIT)
-        	mz_add_definition("WIN32_MINGW64=1")
-    	else()
-    	    mz_add_definition("WIN32_MINGW=1")
-    	endif()
+    if(MZ_64BIT)
+      mz_add_definition("WIN32_MINGW64=1")
+    else()
+      mz_add_definition("WIN32_MINGW=1")
+    endif()
 	endif()
 
 elseif(MZ_IS_VS)
@@ -370,22 +349,22 @@ elseif(MZ_IS_VS)
 endif()
 
 if(MZ_HAS_CXX11)
-    mz_add_definition(MZ_HAS_CXX11=1)
-    mz_add_definition(MZ_HAS_CXX0X=1)
+  mz_add_definition(MZ_HAS_CXX11=1)
+  mz_add_definition(MZ_HAS_CXX0X=1)
 endif()
 
 # On Windows, MS Visual Studio will ignore the
 # environment variables INCLUDE and LIB by default. This forces
 # them to be used
 if(WINDOWS)
-    FOREACH(_inc $ENV{INCLUDE})
-		file(TO_CMAKE_PATH ${_inc} _inc)
-        include_directories("${_inc}")
-        mz_debug_message("Including ${_inc} for headers")
-    ENDFOREACH(_inc)
-    FOREACH(_lib $ENV{LIB})
-		file(TO_CMAKE_PATH ${_lib} _lib)
-        link_directories("${_lib}")
-        mz_debug_message("Including ${_lib} for libs")
-    ENDFOREACH(_lib)	
+  foreach(_inc $ENV{INCLUDE})
+    file(TO_CMAKE_PATH ${_inc} _inc)
+    include_directories("${_inc}")
+    mz_debug_message("Including ${_inc} for headers")
+  endforeach(_inc)
+  foreach(_lib $ENV{LIB})
+    file(TO_CMAKE_PATH ${_lib} _lib)
+    link_directories("${_lib}")
+    mz_debug_message("Including ${_lib} for libs")
+  endforeach(_lib)	
 endif()
--- build/genVS_ProjectFiles.bat
+++ build/genVS_ProjectFiles.bat
@@ -1,27 +1,4 @@
 @ echo off
-REM REM
-REM Copyright (c) 2008-2012 Marius Zwicker
-REM All rights reserved.
-REM 
-REM @LICENSE_HEADER_START:Apache@
-REM 
-REM Licensed under the Apache License, Version 2.0 (the "License");
-REM you may not use this file except in compliance with the License.
-REM You may obtain a copy of the License at
-REM 
-REM     http://www.apache.org/licenses/LICENSE-2.0
-REM 
-REM Unless required by applicable law or agreed to in writing, software
-REM distributed under the License is distributed on an "AS IS" BASIS,
-REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-REM See the License for the specific language governing permissions and
-REM limitations under the License.
-REM 
-REM http://www.mlba-team.de
-REM 
-REM @LICENSE_HEADER_END:Apache@
-REM REM
-
 REM #############################################################
 REM
 REM		Configure for Visual Studio 2003
