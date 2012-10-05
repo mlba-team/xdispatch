d5699e72b161daf67a1ed337d92fd36a162f03f0
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-10-05 11:19:56 +0000

    Update to mz-cmaketools (r459)
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/trunk@461 9e4c620c-c391-4b63-a7fb-4924991ea895

--- build/global.cmake
+++ build/global.cmake
@@ -1,26 +1,3 @@
-##
-# Copyright (c) 2008-2012 Marius Zwicker
-# All rights reserved.
-# 
-# @LICENSE_HEADER_START:Apache@
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
-# http://www.mlba-team.de
-# 
-# @LICENSE_HEADER_END:Apache@
-##
-
 ##################################################
 #
 #	BUILD/GLOBAL.CMAKE
@@ -31,6 +8,8 @@
 #	be included twice or more without any issues and
 #   will automatically included the utility files 
 #   compiler.cmake and macros.cmake
+#
+#	(c) 2009-2012 Marius Zwicker
 #
 ##################################################
 
