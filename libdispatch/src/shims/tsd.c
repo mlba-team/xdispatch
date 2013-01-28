6b754cdddb1a376fc64d0d0e332d963cd10eca7a
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2013-01-28 09:05:02 +0000

    Updated copyright information to 2013
    git-svn-id: http://opensource.mlba-team.de/svn/xdispatch/trunk@564 9e4c620c-c391-4b63-a7fb-4924991ea895

--- libdispatch/src/shims/tsd.c
+++ libdispatch/src/shims/tsd.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2010 Apple Inc. All rights reserved.
- * Copyright (c) 2011-2012 MLBA-Team. All rights reserved.
+ * Copyright (c) 2011-2013 MLBA-Team. All rights reserved.
  *
  * @APPLE_APACHE_LICENSE_HEADER_START@
  *
